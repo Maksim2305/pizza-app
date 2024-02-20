30d52dd76d1f823639b4c037bf037dd6cdbec208
mzhurin <mzhurin@phoenixit.ru>
2024-02-20 22:55:15 +0300

    добавил стили, настройки eslint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
-import { ButtonProps } from "../Button.props";
-import cn from "classnames";
-import styles from "./Button.module.css";
-function Button({ children, className, ...props }: ButtonProps) {
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
+import styles from './Button.module.css';
+function Button({ children, className, appearence = 'small', ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className={cn(styles["button"], className)}
+      className={cn(styles['button'],{
+        [styles['small']]: appearence ==='small',
+        [styles['big']]: appearence ==='big'
+      }, className)}
     >
       {children}
     </button>
